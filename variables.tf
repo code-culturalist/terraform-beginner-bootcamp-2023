f015d5b3c566c78e5fe32d463a4fce0bd70abac5
code-culturalist <144938614+code-culturalist@users.noreply.github.com>
2023-09-29 16:42:36 -0600

    #23 Bucket should no longer use random provider (#24)

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable "user_uuid" {
     error_message = "User UUID must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426614174000)"
   }
 }
+
+variable "bucket_name" {
+  type        = string
+  description = "The name of the AWS S3 bucket"
+  
+  validation {
+    condition     = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid S3 bucket name. Bucket names must be between 3 and 63 characters long and can only contain lowercase letters, numbers, hyphens, and periods."
+  }
+}
